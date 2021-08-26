8337743e55fc85d0e2df1e37eff8a558ba4d087c
Varun Prabhakaran <varunprabhakaran2209@gmail.com>
2021-08-26 13:27:14 +0530

    add env and keys

---
+++ Server/keys/algolia.js
@@ -0,0 +1,6 @@
+module.exports = {
+    algolia: {
+        APP_ID:"E6A0E7IL4I",
+        ADMIN_KEY:"44e95762430067851b4126d7611dbb49"
+    }
+}
---
+++ Server/keys/googleClientId.js
@@ -0,0 +1,5 @@
+const GOOGLE_CLIENT_ID = "809179869790-649i31s3m0jph26g46apmadd3a5j461j.apps.googleusercontent.com";
+
+module.exports = {
+    GOOGLE_CLIENT_ID,
+};
