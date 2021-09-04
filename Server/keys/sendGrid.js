3b64c5c1294d819d82cc5a4f7b4eb6694bb9c02e
N6Zeta <89178568+N6Zeta@users.noreply.github.com>
2021-09-04 16:30:31 +0530

    Delete Server/keys directory

--- Server/keys/sendGrid.js
+++
@@ -1,7 +0,0 @@
-const SENDGRID_API_KEY = "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";
-const senderEmail = "ninja6teamzeta@gmail.com";
-
-module.exports = {
-    SENDGRID_API_KEY,
-    senderEmail,
-};
